5035ac44846bf1a293c1cadf196cf42594f85789
Steven Than <steventhan11@gmail.com>
2018-12-07 15:59:18 -0500

    solutions removed

--- class_ex/guinea_pig.py
+++ class_ex/guinea_pig.py
@@ -8,50 +8,5 @@ Expand on the class GuineaPig:
     4. Can you retrofit the class so we can add 2 guinea pigs to make a super pig?
 """
 
-
 class GuineaPig:
-    def __init__(self, name, color, age, weight, fav_level):
-        self.name = name
-        self.age = age
-        self.weight = weight
-        self.color = color
-        self.fav_level = fav_level
-    
-    def jump(self):
-        if self.weight > 2:
-            print("Too heavy, can't jump, sorry!")
-            return False
-        else:
-            return True
-    
-    def __eq__(self, another_pig):
-        return self.age == another_pig.age \
-           and self.weight == another_pig.weight
-    
-    def __gt__(self, another_pig):
-        if self.age > another_pig.age:
-            return True
-        elif self.age == another_pig.age:
-            return self.weight > another_pig.weight
-
-    def __ge__(self, another_pig):
-        return self.__gt__(another_pig) or self.__eq__(another_pig)
-    
-    def __lt__(self, another_pig):
-        if self.age < another_pig.age:
-            return True
-        elif self.age == another_pig.age:
-            return self.weight < another_pig.weight
-
-    def __le__(self, another_pig):
-        return self.__lt__(another_pig) or self.__eq__(another_pig)
-
-
-    def __add__(self, another_pig):
-        new_name = "{} {}".format(self.name, another_pig.name)
-        new_age = self.age + another_pig.age
-        new_weight = self.weight + another_pig.weight
-        new_color = "{}/{}" .format(self.color, another_pig.color)
-        new_fav_level = (self.fav_level + another_pig.fav_level) // 2
-        new_pig = GuineaPig(new_name, new_color, new_age, new_weight, new_fav_level)
-        return new_pig
+    pass
--- list_ex/list_ex.py
+++ list_ex/list_ex.py
@@ -8,16 +8,7 @@ Example:
 """
 
 def convert_to_int_list(lst):
-    ret = []
-
-    for element in lst:
-        if isinstance(element, (int, float, complex)) \
-            and type(element) != bool:
-            ret.append(element)
-        else:
-            ret.append(0)
-
-    return ret
+    pass
 
 """
 2. Design a function that flattens a 2D list.
@@ -31,11 +22,7 @@ Output: [1, 2, 3, 4, 5, 6, 7 , 8, 9]
 """
 
 def flatten_2d_lst(lst):
-    ret = []
-    for row in lst:
-        for element in row:
-            ret.append(element)
-    return ret
+    pass
 
 
 """
@@ -46,16 +33,7 @@ Output: 1
 """
 
 def reverse_and_double_string(string, letter):
-    if not string:
-        return ""
-    else:
-        rev = reverse_and_double_string(string[1:], letter)
-        current = string[0]
-        if current == letter:
-            new_list = rev + current * 2
-        else:
-            new_list = rev + current
-        return new_list
+    pass
 
 
 """
@@ -69,15 +47,3 @@ class GuineaPig:
         self.name = name
         self.fav_level = fav_level
 
-
-def fav_pigs(pigs: List[GuineaPig]) -> List:
-    if not pigs:
-        return []
-    else:
-        prev_list = fav_pigs(pigs[1:])
-        current_pig = pigs[0]
-        if current_pig.fav_level >= 3:
-            new_list = [current_pig] + prev_list
-        else:
-            new_list = prev_list
-        return new_list
